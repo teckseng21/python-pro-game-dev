2d629267e1b42ffdb62a053dc1534a6d9cbe6a6f
teckseng21 <teckseng21@gmail.com>
2026-06-13 10:58:26 +0800

    Add files via upload

---
+++ ball.py
@@ -0,0 +1,24 @@
+import pgzrun
+
+WIDTH = 800
+HEIGHT=600
+TITLE="Flappy Ball"
+
+class Ball:
+    def __init__(self, initial_x, initial_y):
+        self.x = initial_x
+        self.y = initial_y
+        self.radius=40
+        self.vx=200
+        self.vy=0
+    
+    def draw(self):
+        pos=(self.x,self.y)
+        screen.draw.circle(pos,self.radius,"blue")
+        
+
+ball=Ball(50,100)
+screen.clear()
+ball.draw()
+ball.display()
+pgzrun.go()
